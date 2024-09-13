a81c1c080fc19d4579763533da0d56c6218df744
Cristian Andres <barcandres91@gmail.com>
2024-09-13 15:28:06 -0500

    se crea pantalla de roles

---
+++ inversiones/lib/src/ui/pages/roles/roles_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:inversiones/src/ui/pages/widgets/appbar/app_bar_custom.dart';
+
+class RolesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(appBar: AppBarCustom('Roles'),);
+  }
+}
