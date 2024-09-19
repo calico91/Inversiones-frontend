163521493c0f92c240e7330ad57473c33d283cf7
Cristian Andres <barcandres91@gmail.com>
2024-09-19 17:03:26 -0500

    se crea entidad permisos y se anexa a la entidad de roles

---
+++ inversiones/lib/src/domain/entities/permiso.dart
@@ -0,0 +1,34 @@
+class Permiso {
+  const Permiso(
+      {required this.id, required this.descripcion, required this.endpoint});
+
+  final int id;
+  final String descripcion;
+  final String endpoint;
+
+  factory Permiso.fromJson(Map<String, dynamic> json) => Permiso(
+      id: json['id'] as int,
+      descripcion: json['descripcion'] as String,
+      endpoint: json['endpoint'] as String);
+
+  Map<String, dynamic> toJson() {
+    return {
+      if (id != 0) 'id': id,
+      'descripcion': descripcion,
+      'endpoint': endpoint
+    };
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Permiso &&
+        other.id == id &&
+        other.descripcion == descripcion &&
+        other.endpoint == endpoint;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ descripcion.hashCode ^ endpoint.hashCode;
+}
--- inversiones/lib/src/ui/pages/roles/roles_page.dart
+++ inversiones/lib/src/ui/pages/roles/roles_page.dart
@@ -17,47 +17,14 @@ class RolesPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Center(
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: const Text('Asignar permisos',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),),
-          ),
+              child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('Asignar permisos',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 17)))),
           Obx(
             () => !controller.cargando.value
-                ? SizedBox(
-                    width: double.infinity,
-                    child: CustomCard(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            width: mediaQuery.width * 0.39,
-                            height: mediaQuery.height * 0.06,
-                            child: DropdownButton<String>(
-                              isExpanded: true,
-                              items: [
-                                const DropdownMenuItem<String>(
-                                  child: Text('Seleccione un rol'),
-                                ),
-                                ...controller.roles.value.map((role) =>
-                                    DropdownMenuItem<String>(
-                                        value: role.id.toString(),
-                                        child: Text(role.name)))
-                              ],
-                              value: controller.rolSeleccionado.value.isEmpty
-                                  ? null
-                                  : controller.rolSeleccionado.value,
-                              onChanged: (String? newValue) {
-                                controller.rolSeleccionado.value =
-                                    newValue ?? '';
-                                if (newValue != null) {
-                                  print('voy hacer la peticion');
-                                }
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
+                ? _selectRoles(mediaQuery, controller)
                 : const LinearProgressIndicator(),
           )
         ],
@@ -65,3 +32,31 @@ class RolesPage extends StatelessWidget {
     );
   }
 }
+
+Widget _selectRoles(Size mediaQuery, RolesController controller) => SizedBox(
+    width: double.infinity,
+    child: CustomCard(
+        child: Column(children: [
+      SizedBox(
+          width: mediaQuery.width * 0.8,
+          height: mediaQuery.height * 0.06,
+          child: DropdownButton<String>(
+              isExpanded: true,
+              items: [
+                const DropdownMenuItem<String>(
+                  child: Text('Seleccione un rol'),
+                ),
+                ...controller.roles.value.map((role) =>
+                    DropdownMenuItem<String>(
+                        value: role.id.toString(), child: Text(role.name)))
+              ],
+              value: controller.rolSeleccionado.value.isEmpty
+                  ? null
+                  : controller.rolSeleccionado.value,
+              onChanged: (String? newValue) {
+                controller.rolSeleccionado.value = newValue ?? '';
+                if (newValue != null) {
+                  print('voy hacer la peticion');
+                }
+              }))
+    ])));
