9f78e0094ae8820dc3978d9ff394c5d98b383898
Cristian Andres <barcandres91@gmail.com>
2024-09-23 10:23:11 -0500

    se modifica formateo en archivo

--- inversiones/lib/src/ui/pages/pay_fee/pay_fee_page.dart
+++ inversiones/lib/src/ui/pages/pay_fee/pay_fee_page.dart
@@ -24,27 +24,21 @@ class PayFeePage extends StatelessWidget {
       appBar: const AppBarCustom('Cuota credito'),
       body: Padding(
         padding: EdgeInsets.symmetric(
-          vertical: General.mediaQuery(context).height * 0.03,
-          horizontal: General.mediaQuery(context).width * 0.1,
-        ),
+            vertical: General.mediaQuery(context).height * 0.03,
+            horizontal: General.mediaQuery(context).width * 0.1),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              SizedBox(
                   width: General.mediaQuery(context).width * 0.5,
                   child: Text(controller.nombreCliente,
                       style: const TextStyle(
                           fontSize: 20, overflow: TextOverflow.ellipsis),
-                      maxLines: 2),
-                ),
-                ShareButton(
+                      maxLines: 2)),
+              ShareButton(
                   screenshotController: screenshotController,
-                  descripcion: 'Informacion cuota ${controller.nombreCliente}',
-                ),
-              ],
-            ),
+                  descripcion: 'Informacion cuota ${controller.nombreCliente}')
+            ]),
             const SizedBox(height: 10),
             Screenshot(
                 controller: screenshotController,
@@ -52,58 +46,44 @@ class PayFeePage extends StatelessWidget {
                     child: Obx(() => ColoredBox(
                         color: Colors.white,
                         child: Column(children: [
+                          _infoValorCuota(General.mediaQuery(context),
+                              'Fecha cuota', controller.payFee.fechaCuota!),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Fecha cuota',
-                            controller.payFee.fechaCuota!,
-                          ),
+                              General.mediaQuery(context),
+                              'Proxima Mora',
+                              controller.payFee.fechaProximaMora!),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Proxima Mora',
-                            controller.payFee.fechaProximaMora!,
-                          ),
+                              General.mediaQuery(context),
+                              'Cantidad cuotas',
+                              controller.payFee.numeroCuotas!.toString()),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Cantidad cuotas',
-                            controller.payFee.numeroCuotas!.toString(),
-                          ),
+                              General.mediaQuery(context),
+                              'Cuota numero',
+                              controller.payFee.cuotaNumero!.toString()),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Cuota numero',
-                            controller.payFee.cuotaNumero!.toString(),
-                          ),
+                              General.mediaQuery(context),
+                              'Dias mora',
+                              controller.payFee.diasMora.toString()),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Dias mora',
-                            controller.payFee.diasMora.toString(),
-                          ),
+                              General.mediaQuery(context),
+                              'Interes mora',
+                              General.formatoMoneda(
+                                  controller.payFee.interesMora)),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Interes mora',
-                            General.formatoMoneda(
-                              controller.payFee.interesMora,
-                            ),
-                          ),
+                              General.mediaQuery(context),
+                              'Valor interes',
+                              General.formatoMoneda(
+                                  controller.payFee.valorInteres)),
                           _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Valor interes',
-                            General.formatoMoneda(
-                              controller.payFee.valorInteres,
-                            ),
-                          ),
-                          _infoValorCuota(
-                            General.mediaQuery(context),
-                            'Valor capital',
-                            General.formatoMoneda(
-                              controller.payFee.valorCapital,
-                            ),
-                          ),
+                              General.mediaQuery(context),
+                              'Valor capital',
+                              General.formatoMoneda(
+                                  controller.payFee.valorCapital)),
                           _infoValorCuota(
                               General.mediaQuery(context),
                               'Valor cuota',
                               General.formatoMoneda(
-                                controller.payFee.valorCuota,
-                              ))
+                                  controller.payFee.valorCuota))
                         ]))))),
 
             ///botones
@@ -143,147 +123,97 @@ class PayFeePage extends StatelessWidget {
     );
   }
 
-  Widget _infoValorCuota(
-    Size size,
-    String titulo,
-    String info,
-  ) {
-    return Padding(
+  Widget _infoValorCuota(Size size, String titulo, String info) => Padding(
       padding: EdgeInsets.symmetric(
-        vertical: size.height * 0.01,
-        horizontal: size.width * 0.01,
-      ),
-      child: Row(
-        children: [
-          Text(
-            '$titulo:',
-            style: const TextStyle(fontSize: 20, color: Colors.blueGrey),
-          ),
-          Expanded(child: Container()),
-          Text(
-            info,
-            style: const TextStyle(fontSize: 20),
-          ),
-        ],
-      ),
-    );
-  }
+          vertical: size.height * 0.01, horizontal: size.width * 0.01),
+      child: Row(children: [
+        Text('$titulo:',
+            style: const TextStyle(fontSize: 20, color: Colors.blueGrey)),
+        Expanded(child: Container()),
+        Text(info, style: const TextStyle(fontSize: 20))
+      ]));
 
-  Future _mostrarConfirmacionPagoCuota(
-    Size size,
-    String mensaje,
-    BuildContext context,
-    PayFeeController controller,
-    bool soloInteres,
-  ) {
+  Future _mostrarConfirmacionPagoCuota(Size size, String mensaje,
+      BuildContext context, PayFeeController controller, bool soloInteres) {
     soloInteres
         ? controller.valorAbono.text =
             controller.payFee.valorInteres.toString().split(".").first
         : controller.valorAbono.text = '';
 
     return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        scrollable: true,
-        actionsPadding: EdgeInsets.zero,
-        content: soloInteres
-            ? SizedBox(
-                height: size.height * 0.17,
-                child: Column(
-                  children: [
-                    Text(
-                      textAlign: TextAlign.center,
-                      mensaje,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Form(
-                      key: controller.formKey,
-                      child: TextFieldBase(
-                        title: 'valor Interes',
-                        controller: controller.valorAbono,
-                        textInputType: TextInputType.number,
-                        validateText: ValidateText.creditValue,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            : Obx(
-                () => SizedBox(
-                  height: controller.cambiarCuota.value
-                      ? size.height * 0.2
-                      : size.height * 0.08,
-                  child: Column(
-                    children: [
-                      Text(
-                        textAlign: TextAlign.center,
-                        mensaje,
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            textAlign: TextAlign.center,
-                            'Modificar cuota',
-                          ),
-                          Switch(
-                            value: controller.cambiarCuota.value,
-                            activeColor: Colors.blue,
-                            onChanged: (bool value) =>
-                                controller.cambiarValorSwitch(value),
-                          ),
-                        ],
-                      ),
-                      if (controller.cambiarCuota.value)
-                        Form(
-                          key: controller.formKey,
-                          child: TextFieldBase(
-                            title: 'Valor cuota',
-                            controller: controller.valorAbono,
-                            textInputType: TextInputType.number,
-                            validateText: ValidateText.creditValue,
-                          ),
-                        )
-                      else
-                        const SizedBox(),
-                    ],
-                  ),
-                ),
-              ),
-        actions: [
-          TextButton(
-            child: const Text('Si'),
-            onPressed: () {
-              if (!soloInteres) {
-                if (controller.cambiarCuota.value) {
-                  if (General.validateForm(controller.formKey)) {
-                    controller.pagarCuota(Constantes.CUOTA_NORMAL);
-                    Navigator.pop(context);
-                    return;
-                  }
-                } else {
-                  controller.pagarCuota(Constantes.CUOTA_NORMAL);
-                  Navigator.pop(context);
-                  return;
-                }
-              } else {
-                if (General.validateForm(controller.formKey)) {
-                  controller.pagarCuota(Constantes.SOLO_INTERES);
-                  Navigator.pop(context);
-                }
-              }
-            },
-          ),
-          TextButton(
-            child: const Text('No'),
-            onPressed: () {
-              Navigator.pop(context);
-              controller.cambiarCuota(false);
-            },
-          ),
-        ],
-      ),
-    );
+        context: context,
+        builder: (context) => AlertDialog(
+                scrollable: true,
+                actionsPadding: EdgeInsets.zero,
+                content: soloInteres
+                    ? SizedBox(
+                        height: size.height * 0.17,
+                        child: Column(children: [
+                          Text(textAlign: TextAlign.center, mensaje),
+                          const SizedBox(height: 20),
+                          Form(
+                              key: controller.formKey,
+                              child: TextFieldBase(
+                                  title: 'valor Interes',
+                                  controller: controller.valorAbono,
+                                  textInputType: TextInputType.number,
+                                  validateText: ValidateText.creditValue))
+                        ]))
+                    : Obx(() => SizedBox(
+                        height: controller.cambiarCuota.value
+                            ? size.height * 0.2
+                            : size.height * 0.08,
+                        child: Column(children: [
+                          Text(textAlign: TextAlign.center, mensaje),
+                          Row(children: [
+                            const Text(
+                                textAlign: TextAlign.center, 'Modificar cuota'),
+                            Switch(
+                                value: controller.cambiarCuota.value,
+                                activeColor: Colors.blue,
+                                onChanged: (bool value) =>
+                                    controller.cambiarValorSwitch(value))
+                          ]),
+                          if (controller.cambiarCuota.value)
+                            Form(
+                                key: controller.formKey,
+                                child: TextFieldBase(
+                                    title: 'Valor cuota',
+                                    controller: controller.valorAbono,
+                                    textInputType: TextInputType.number,
+                                    validateText: ValidateText.creditValue))
+                          else
+                            const SizedBox()
+                        ]))),
+                actions: [
+                  TextButton(
+                      child: const Text('Si'),
+                      onPressed: () {
+                        if (!soloInteres) {
+                          if (controller.cambiarCuota.value) {
+                            if (General.validateForm(controller.formKey)) {
+                              controller.pagarCuota(Constantes.CUOTA_NORMAL);
+                              Navigator.pop(context);
+                              return;
+                            }
+                          } else {
+                            controller.pagarCuota(Constantes.CUOTA_NORMAL);
+                            Navigator.pop(context);
+                            return;
+                          }
+                        } else {
+                          if (General.validateForm(controller.formKey)) {
+                            controller.pagarCuota(Constantes.SOLO_INTERES);
+                            Navigator.pop(context);
+                          }
+                        }
+                      }),
+                  TextButton(
+                      child: const Text('No'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        controller.cambiarCuota(false);
+                      })
+                ]));
   }
 }
